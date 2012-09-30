a2a5a29a15ed7ffd2c86a954eda8220fffd14574
Josh Matthews <jmatth@nbcs.rutgers.edu>
2012-09-30 05:13:18 -0400

    added adhoc script

--- ad-hoc.sh
+++ ad-hoc.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+ifconfig wlan0 down
+iwconfig wlan0 mode ad-hoc
+iwconfig wlan0 essid "Pi"
+ifconfig wlan0 10.0.0.1 netmask 255.255.255.0
+ifconfig wlan0 up
 
 iptables-restore < iptables.conf
 
