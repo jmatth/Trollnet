c27aa7e3d80f8356d1ea0240c83487e194809999
Josh Matthews <jmatth@nbcs.rutgers.edu>
2012-09-30 05:53:50 -0400

    adding mongodriver

--- mongodriver.py
+++ mongodriver.py
@@ -12,20 +12,36 @@ db = connection.trollfi
 
 collect = db.users
 
-post = { "last" : "" }
+stdin = sys.stdin.readline()
 
+out = db.users.find({}, { "last": 1 });
+for item in out:
+	if item["last"] != "":
+		print item["last"]
+	else:
+		commands.getoutput('/home/josh/HackNY2012/russ.pl')
+
+db.users.remove({})
+
+post = { "last" : stdin }
 collect.insert(post)
 
-while True:	
-	stdin = sys.stdin.readline()
-	for line in stdin:
-		out = db.users.find({}), { "last", 1 }
-		if out != "":
-			print out
-		else:
-			commands.getoutput('/home/josh/HackNY2012/cats.pl')
-		ran = random.randint(0, 10)
-		if ran<=4:
-			stdin=""
-		post = { "last" : stdin }
-		collect.insert(post)
+#post = { "last" : "" }
+
+#collect.insert(post)
+
+#while True:	
+#	stdin = sys.stdin.readline()
+#	for line in stdin:
+#		out = db.users.find({}, { "last": 1 });
+#		for item in out:
+#			if item["last"] != "":
+#				print item["last"]
+#			else:
+#				commands.getoutput('/home/josh/HackNY2012/russ.pl')
+#		#ran = random.randint(0, 10)
+#		#if ran<=4:
+#		#	stdin=""
+#		db.users.remove({});
+#		post = { "last" : stdin }
+#		collect.insert(post)
