a47b7e5a83ed99af5c5a9efab38173e34062ca01
Josh Matthews <jmatth@nbcs.rutgers.edu>
2012-11-13 20:37:43 -0500

    removing specific domain from troll.pl

--- pi/troll.pl
+++ pi/troll.pl
@@ -7,7 +7,7 @@ while (<>) {
 	chomp $_;
 	if ($_ =~ /(.*\.jpg)/i || $_ =~ /(.*\.png)/i || $_ =~ /(.*\.gif)/i ) {
 		$url = $1;
-		print "http://scratch.russfrank.us/~russfrank/hackny/cat.php?i=$url\n";
+		print "http://yourdomain.com/path/to/troll.php?i=$url\n";
 	} else {
 		print "$_\n";;
 	}
